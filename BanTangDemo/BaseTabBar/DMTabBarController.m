7868f875b480e941be3949fa0e28a8059bfa6671
devmao <1139476538@qq.com>
2017-07-21 10:58:45 +0800

    add PrefixHeader.pch  and UITabbarViewController.

--- BanTangDemo/BaseTabBar/DMTabBarController.m
+++ BanTangDemo/BaseTabBar/DMTabBarController.m
@@ -17,14 +17,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
    
-    self.tabBar.backgroundImage  = [UIImage imageNamed:@"tab_bar_bg"];
+    //背景颜色
+    self.tabBar.backgroundImage  = [UIImage imageNamed:@"tabbar_back.png"];
+    
+    //默认的字体颜色
     UIColor * tabBarTitleColor = [UIColor colorWithRed:102/255.0 green:102/255.0 blue:102/255.0 alpha:1];
     [[UITabBarItem appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
                                                        tabBarTitleColor, NSForegroundColorAttributeName,
                                                        nil] forState:UIControlStateNormal];
+    //选中时的字体颜色
     [[UITabBarItem appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
-                                                       [UIColor redColor], NSForegroundColorAttributeName,
+                                                       MainColor, NSForegroundColorAttributeName,
                                                        nil] forState:UIControlStateSelected];
+    //首页
     [self setupFirstChildVC:[[DMHomePageController alloc]init] title:@"首页" image:@"tab_首页" selectedImage:@"tab_首页_pressed"];
 }
 
@@ -34,8 +39,8 @@
     [self addChildViewController:vc];
     // 设置子控制器的tabBarItem
     vc.tabBarItem.title         = title;
-    vc.tabBarItem.image         = [[UIImage imageNamed:image] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
-    vc.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImage] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    vc.tabBarItem.image         = [[UIImage imageNamed:image] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    vc.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImage] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     
 }
 @end
